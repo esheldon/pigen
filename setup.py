b4b4f05d3e3f88e8ef07d1dc46e6bc17e95dd306
Erin Sheldon <erin.sheldon@gmail.com>
2017-09-26 11:37:11 -0400

    added pigen script

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 import distutils
 from distutils.core import setup
 
+scripts=['pigen']
+scripts=[os.path.join('bin',s) for s in scripts]
+
 setup(
     name="pigen", 
     packages=['pigen'],
     version="0.1.0",
+    scripts=scripts,
 )
-
 
 
 
