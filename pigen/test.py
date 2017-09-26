105bb759dbc23ba27004da670d95d85efee763bf
Erin Sheldon <erin.sheldon@gmail.com>
2017-09-26 09:29:23 -0400

    remove dead code

--- pigen/test.py
+++ pigen/test.py
@@ -13,13 +13,19 @@ def test(prefix_var_names=False):
         ],
 
         'functions': [
+
             {'def':'void noarg_or_return(void)',
              'doc':'no argument or return value'},
+
             'int noarg()',
+
             'float scalar(float x);',
+
             'double fdouble(double * y, size_t ny);',
+
             {'def':'void fill_fdiff(struct gauss* gmix, long n_gauss, double *fdiff, long n_fdiff);',
              'doc':'unpack user define struct array and double array'},
+
             {'def':'PyObject* fpyobj(PyObject* input1, PyObject* input2);',
              'doc':'All PyObject*, so the user must deal with extracting data and generating the PyObject* return value'},
         ]
@@ -29,18 +35,3 @@ def test(prefix_var_names=False):
 
     mwrap = module.ModuleWrapper(conf)
     print(mwrap)
-
-    '''
-    for d in conf['functions']:
-        print('// ' + '-'*70)
-        print()
-
-        wrapper=funcwrap.FuncWrapper(
-            funcdef=d,
-            prefix=conf['wrapper_prefix'],
-            prefix_var_names=conf['prefix_var_names'],
-        )
-        print(wrapper)
-        print()
-    '''
-
